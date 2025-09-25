f4b190876eab6eb0888aa4cac04f04c5e6558e7d
Yvan Razafindramanana <mail@ludeo.net>
2025-09-25 09:20:57 +0200

    feat(api): healthcheck

---
+++ api/src/functions/health.ts
@@ -0,0 +1,12 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function health(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+	context.log(`Http function processed request for url "${request.url}"`);
+	return { body: 'Healthy' };
+};
+
+app.http('health', {
+	methods: ['GET'],
+	authLevel: 'anonymous',
+	handler: health
+});
